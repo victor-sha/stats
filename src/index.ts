0ba41bf25c947c23cbd63c04a1724242e6620146
victor-sha <kassini_90@mail.ru>
2019-08-04 13:07:03 +0300

    make CsvFileReader abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import fs from 'fs';
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResults';
+import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
